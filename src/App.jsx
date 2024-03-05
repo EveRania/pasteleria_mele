77a0528bb3f0f1acfb701a13bd51ffeddb0c9c95
Evelyn Raña <ev.r@live.com.ar>
2024-03-05 18:10:39 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import './App.css'
+import NavBar from './components/navBar/NavBar'
+import ItemListContainer from './components/itemListContainer/ItemListContainer'
+
+function App() {
+
+  return (
+    <ChakraProvider>
+      <NavBar />
+      <ItemListContainer title='Tienda'/>
+    </ChakraProvider>
+  )
+}
+
+export default App
